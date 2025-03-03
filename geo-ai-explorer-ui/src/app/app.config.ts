9a0a6441213eea5aa2e45f10fa76a76999b73838
Richard Rowlands <rrowlands@terraframe.com>
2025-03-03 16:11:52 -0700

    Switch from bootstrap to prime ng

--- geo-ai-explorer-ui/src/app/app.config.ts
+++ geo-ai-explorer-ui/src/app/app.config.ts
@@ -1,9 +1,21 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideRouter } from '@angular/router';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 
 import { routes } from './app.routes';
 
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations()]
+    providers: [
+        provideRouter(routes), provideAnimations(),
+        provideAnimationsAsync(),
+        providePrimeNG({
+            theme: {
+                preset: Aura
+            }
+        })
+    ]
 };
